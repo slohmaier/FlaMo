14276a7d168f67dd8457c8ae56680acf30056fd7
Stefan Lohmaier <stefan@noneus.com>
2016-06-11 14:18:55 +0200

    Working login system with global app password using flask-login module

--- flamo.py
+++ flamo.py
@@ -23,15 +23,78 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+import os
+
+import flask
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+from flask.ext.login import LoginManager, UserMixin
+from flask.ext.login import login_required, login_user, logout_user
+from easysettings import EasySettings
 
+from flashforge import FlashForge
+
+#some default values
+DEFAULT_PASSWORD = 'flamo'
+
+#create the app
 app = Flask('flamo')
+app.config['SECRET_KEY'] = os.environ.get('FLAMO_SECRET_KEY', 'flamo')
+
+#login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+#socket io server
 socketio = SocketIO(app)
 
+#settings
+settings = EasySettings('flamo.conf')
+
+#printer
+#ff = FlashForge()
+
+#dummy user class for flask-login
+class User(UserMixin):
+	def get_id(self):
+		return 'user'
+
+@login_manager.user_loader
+def load_user(id):
+	return User()
+
+@login_manager.request_loader
+def load_user_request(request):
+	token = request.headers.get('Authorization')
+	if token is None:
+		token = request.args.get('token')
+	
+	if token == settings.get('password'):
+		return User()
+	else:
+		return None
+
+#non socketio routes
 @app.route('/', methods=['GET'])
+@login_required
 def index():
 	return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		request.form['password'] == settings.get('password', 'flamo')
+		login_user(User())
+		return flask.redirect('/')
+	
+	return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flask.redirect('/login')
 
 if __name__ == '__main__':
 	socketio.run(app, debug=True)
