ca91a179ebb8e79fb2869a6f7858187815d1187c
Stefan Lohmaier <stefan@noneus.com>
2016-09-05 20:09:38 +0200

    Now sending GCodes for Move buttons.

--- static/js/controls.js
+++ static/js/controls.js
@@ -11,59 +11,89 @@ $('#move_xy_home').click(function(){
 	socket.emit('gcodecmd', 'G28 X Y');
 });
 
+function gcode_G1(cmd) {
+	socket.emit('gcodecmd', 'G91');
+	socket.emit('gcodecmd', 'G1 ' + cmd);
+	socket.emit('gcodecmd', 'G90');
+}
+
 $('#move_x_up_100').click(function(){
+	gcode_G1('X+100');
 });
 $('#move_x_up_10').click(function(){
+	gcode_G1('X+10');
 });
 $('#move_x_up_1').click(function(){
+	gcode_G1('X+1');
 });
 $('#move_x_up_01').click(function(){
+	gcode_G1('X+0.1');
 });
 $('#move_x_down_01').click(function(){
+	gcode_G1('X-0.1');
 });
 $('#move_x_down_1').click(function(){
+	gcode_G1('X-1');
 });
 $('#move_x_down_10').click(function(){
+	gcode_G1('X-10');
 });
 $('#move_x_down_100').click(function(){
+	gcode_G1('X-100');
 });
 
 $('#move_y_up_100').click(function(){
+	gcode_G1('Y+100');
 });
 $('#move_y_up_10').click(function(){
+	gcode_G1('Y+10');
 });
 $('#move_y_up_1').click(function(){
+	gcode_G1('Y+1');
 });
 $('#move_y_up_01').click(function(){
+	gcode_G1('Y+0.1');
 });
 $('#move_y_down_01').click(function(){
+	gcode_G1('Y-0.1');
 });
 $('#move_y_down_1').click(function(){
+	gcode_G1('Y-1');
 });
 $('#move_y_down_10').click(function(){
+	gcode_G1('Y-10');
 });
 $('#move_y_down_100').click(function(){
+	gcode_G1('Y-100');
 });
 
 $('#move_xy_home').click(function(){
-	socket.emit('gcodecmd', 'G28 Z');
+	socket.emit('gcodecmd', 'G28 XY');
 });
 
 $('#move_z_up_100').click(function(){
+	gcode_G1('Z+100');
 });
 $('#move_z_up_10').click(function(){
+	gcode_G1('Z+10');
 });
 $('#move_z_up_1').click(function(){
+	gcode_G1('Z+1');
 });
 $('#move_z_up_01').click(function(){
+	gcode_G1('Z+0.1');
 });
 $('#move_z_down_01').click(function(){
+	gcode_G1('Z-0.1');
 });
 $('#move_z_down_1').click(function(){
+	gcode_G1('Z-1');
 });
 $('#move_z_down_10').click(function(){
+	gcode_G1('Z-10');
 });
 $('#move_z_down_100').click(function(){
+	gcode_G1('Z-100');
 });
 
 $('#move_e_up_100').click(function(){
